c6e3991164f1aea351972da9ea0b5e01f7592f62
Rafael Benevides <benevides@redhat.com>
2017-02-15 21:40:11 -0500

    Fix label definition

--- vars/canaryDeploy.groovy
+++ vars/canaryDeploy.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/groovy
 
 def call(project, app, version){
-    sh "oc new-app -n ${project} --name ${app}-${version} ${app}:${version} -l app=${app}, deploymentconfig=${app}, hystrix.enabled=true"
+    sh "oc new-app -n ${project} --name ${app}-${version} ${app}:${version} -l app=${app},deploymentconfig=${app},hystrix.enabled=true"
 }
