5ea28597c99bec93b2e10254e77bbc09962b47b8
Rafael Benevides <benevides@redhat.com>
2017-02-15 21:02:31 -0500

    Update canaryDeploy.groovy

--- vars/canaryDeploy.groovy
+++ vars/canaryDeploy.groovy
@@ -3,6 +3,6 @@
 def call(project, app, version){
     sh "oc new-app -n ${project} --name ${app}-${version} ${app}:${version} -l app=${app},svc=${app}-canary,hystrix.enabled=true"
     sh "oc patch dc/${app} -n ${project} -p '{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"svc\":\"${app}-canary\"}}}}}'"
-    sh "oc deploy dc/${app} --follow"
+    sh "oc deploy dc/${app} -n ${project} --follow"
     sh "oc patch svc/${app} -n ${project} -p '{\"spec\":{\"selector\":{\"svc\":\"${app}-canary\",\"app\": null, \"deploymentconfig\": null}, \"sessionAffinity\":\"None\"}}'"
 }
