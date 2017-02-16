1d5f2fe6e2009c2062049c49179fe1947678a797
Rafael Benevides <benevides@redhat.com>
2017-02-15 21:37:14 -0500

    Place application under the same app and deploymentconfig label

--- vars/canaryDeploy.groovy
+++ vars/canaryDeploy.groovy
@@ -1,7 +1,5 @@
 #!/usr/bin/groovy
 
 def call(project, app, version){
-    sh "oc new-app -n ${project} --name ${app}-${version} ${app}:${version} -l app=${app},svc=${app}-canary,hystrix.enabled=true"
-    sh "oc patch dc/${app} -n ${project} -p '{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"svc\":\"${app}-canary\"}}}}}'"
-    sh "oc patch svc/${app} -n ${project} -p '{\"spec\":{\"selector\":{\"svc\":\"${app}-canary\",\"app\": null, \"deploymentconfig\": null}, \"sessionAffinity\":\"None\"}}'"
+    sh "oc new-app -n ${project} --name ${app}-${version} ${app}:${version} -l app=${app}, deploymentconfig=${app}, hystrix.enabled=true"
 }
