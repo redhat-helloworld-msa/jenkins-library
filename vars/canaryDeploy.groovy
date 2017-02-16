d432db2107474faee683e76e45fb6712a34bed79
Rafael Benevides <benevides@redhat.com>
2017-02-15 21:30:32 -0500

    Revert wait for deploy

--- vars/canaryDeploy.groovy
+++ vars/canaryDeploy.groovy
@@ -3,6 +3,5 @@
 def call(project, app, version){
     sh "oc new-app -n ${project} --name ${app}-${version} ${app}:${version} -l app=${app},svc=${app}-canary,hystrix.enabled=true"
     sh "oc patch dc/${app} -n ${project} -p '{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"svc\":\"${app}-canary\"}}}}}'"
-    sh "oc deploy dc/${app} -n ${project} --latest --follow"
     sh "oc patch svc/${app} -n ${project} -p '{\"spec\":{\"selector\":{\"svc\":\"${app}-canary\",\"app\": null, \"deploymentconfig\": null}, \"sessionAffinity\":\"None\"}}'"
 }
