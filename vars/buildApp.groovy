a836c930b7604eed234d77fe7506df19787d6c9c
Rafael Benevides <benevides@redhat.com>
2017-02-15 19:22:32 -0500

    Update buildApp.groovy

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -1,6 +1,7 @@
 #!/usr/bin/groovy
 
 def call(project, app){
+    projectSet(project)
     sh "oc new-build -n ${project} --binary --name=${app} -l app=${app} || echo 'Build exists'"
     sh "oc start-build ${app} -n ${project} --from-dir=. --follow"
     deployApp(project, app)
