dccb4e31ba8c8743c060d5b5d512c9320a3d1204
Rafael Benevides <benevides@redhat.com>
2017-02-15 21:45:26 -0500

    Patch the service

--- vars/canaryDeploy.groovy
+++ vars/canaryDeploy.groovy
@@ -2,4 +2,5 @@
 
 def call(project, app, version){
     sh "oc new-app -n ${project} --name ${app}-${version} ${app}:${version} -l app=${app},deploymentconfig=${app},hystrix.enabled=true"
+    sh "oc patch svc/${app} -n ${project} -p '{\"spec\":{\"selector\":{\"app\": \"${app}\", \"deploymentconfig\": null}, \"sessionAffinity\":\"None\"}}'"
 }
