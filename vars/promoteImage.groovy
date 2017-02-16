81937fcb0d5bc4ae128c40df4f04fb68fcb3a7af
Rafael Benevides <benevides@redhat.com>
2017-02-15 19:05:09 -0500

    Update promoteImage.groovy

--- vars/promoteImage.groovy
+++ vars/promoteImage.groovy
@@ -4,5 +4,5 @@ def call(origProject, project, app, tag){
     projectSet(project)
     sh "oc policy add-role-to-user system:image-puller system:serviceaccount:${project}:default -n ${origProject}"
     sh "oc tag ${origProject}/${app}:latest ${project}/${app}:${tag}"
-    deployApp(app)
+    deployApp(project, app)
 }
