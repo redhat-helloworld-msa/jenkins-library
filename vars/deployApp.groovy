4e48f09b63b68a1fb0d652cd3c8aef575a4802b5
Rafael Benevides <benevides@redhat.com>
2017-02-15 19:20:20 -0500

    Update deployApp.groovy

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -3,6 +3,6 @@
 def call(project, app){
     sh "oc new-app ${app} -n ${project} -l app=${app},hystrix.enabled=true || echo 'Aplication already Exists'"
     sh "oc expose service ${app} -n ${project} || echo 'Service already exposed'"
-    sh "oc patch dc/${app} -p '{\"spec\":{\"template\":{\"spec\":{\"containers\":[{\"name\":\"${app}\",\"ports\":[{\"containerPort\": 8778,\"name\":\"jolokia\"}]}]}}}}'"
-    sh "oc patch dc/${app} -p '{\"spec\":{\"template\":{\"spec\":{\"containers\":[{\"name\":\"${app}\",\"readinessProbe\":{\"httpGet\":{\"path\":\"/api/health\",\"port\":8080}}}]}}}}\'"
+    sh "oc patch dc/${app} -n ${project} -p '{\"spec\":{\"template\":{\"spec\":{\"containers\":[{\"name\":\"${app}\",\"ports\":[{\"containerPort\": 8778,\"name\":\"jolokia\"}]}]}}}}'"
+    sh "oc patch dc/${app} -n ${project} -p '{\"spec\":{\"template\":{\"spec\":{\"containers\":[{\"name\":\"${app}\",\"readinessProbe\":{\"httpGet\":{\"path\":\"/api/health\",\"port\":8080}}}]}}}}\'"
 }
