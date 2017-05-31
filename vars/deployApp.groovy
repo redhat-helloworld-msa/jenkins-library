59f8bb3f16592eb7b7ca5acbc4be1e301f9eca80
Rafael Benevides <benevides@redhat.com>
2017-05-31 17:56:23 -0400

    Use tag and imageStream to deploy an app

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/groovy
 
-def call(project, app){
-    sh "oc new-app ${app} -n ${project} -l app=${app},hystrix.enabled=true || echo 'Aplication already Exists'"
+def call(project, app, tag){
+    sh "oc new-app ${app} -n ${project} -l app=${app},hystrix.enabled=true --image-stream="${project}/${app}:${tag} || echo 'Aplication already Exists'"
     sh "oc expose service ${app} -n ${project} || echo 'Service already exposed'"
     sh "oc set probe dc/${app} -n ${project} --readiness --get-url=http://:8080/api/health"
     sh "oc patch dc/${app} -n ${project} -p '{\"spec\":{\"template\":{\"spec\":{\"containers\":[{\"name\":\"${app}\",\"ports\":[{\"containerPort\": 8778,\"name\":\"jolokia\"}]}]}}}}'"
