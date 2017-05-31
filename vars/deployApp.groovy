3ec2a4de5d9e334e34b0e674ad0d2ca2fad002af
Rafael Benevides <benevides@redhat.com>
2017-05-31 17:42:50 -0400

    Use oc set probe to eneable readinessprobe

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -3,6 +3,6 @@
 def call(project, app){
     sh "oc new-app ${app} -n ${project} -l app=${app},hystrix.enabled=true || echo 'Aplication already Exists'"
     sh "oc expose service ${app} -n ${project} || echo 'Service already exposed'"
+    sh "oc set probe dc/${app} -n ${project} --readiness --get-url=http://:8080/api/health"
     sh "oc patch dc/${app} -n ${project} -p '{\"spec\":{\"template\":{\"spec\":{\"containers\":[{\"name\":\"${app}\",\"ports\":[{\"containerPort\": 8778,\"name\":\"jolokia\"}]}]}}}}'"
-    sh "oc patch dc/${app} -n ${project} -p '{\"spec\":{\"template\":{\"spec\":{\"containers\":[{\"name\":\"${app}\",\"readinessProbe\":{\"httpGet\":{\"path\":\"/api/health\",\"port\":8080}}}]}}}}\'"
 }
