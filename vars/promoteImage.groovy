90720b2cbac7f0c3fbee5487893f89c747af8675
Rafael Benevides <benevides@redhat.com>
2017-05-31 17:55:30 -0400

    Use tag to deploy image

--- vars/promoteImage.groovy
+++ vars/promoteImage.groovy
@@ -4,5 +4,5 @@ def call(origProject, project, app, tag){
     projectSet(project)
     sh "oc policy add-role-to-user system:image-puller system:serviceaccount:${project}:default -n ${origProject}"
     sh "oc tag ${origProject}/${app}:latest ${project}/${app}:${tag}"
-    deployApp(project, app)
+    deployApp(project, app, tag)
 }
