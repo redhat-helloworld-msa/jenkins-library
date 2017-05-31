1464a04ec2a921f3bb45c811170c3ab824e99708
Rafael Benevides <benevides@redhat.com>
2017-05-31 18:02:48 -0400

    Use latest tag to build an app

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -4,5 +4,5 @@ def call(project, app){
     projectSet(project)
     sh "oc new-build -n ${project} --binary --name=${app} -l app=${app} || echo 'Build exists'"
     sh "oc start-build ${app} -n ${project} --from-dir=. --follow"
-    deployApp(project, app)
+    deployApp(project, app, 'latest')
 }
