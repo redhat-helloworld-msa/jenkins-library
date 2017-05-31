9f22a6e5e77cd4545a1800fadadaa1000b93078c
Rafael Benevides <benevides@redhat.com>
2017-05-31 17:37:27 -0400

    Define project without credentials

--- vars/projectSet.groovy
+++ vars/projectSet.groovy
@@ -1,10 +1,7 @@
 #!/usr/bin/groovy
 
 def call(project) {
-    //Use a credential called openshift-dev
-    withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'openshift-dev', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-        sh "oc login --insecure-skip-tls-verify=true -u $env.USERNAME -p $env.PASSWORD $OPENSHIFT"
-    }
     sh "oc new-project ${project} || echo 'Project exists'"
     sh "oc project ${project}"
+    sh "oc policy add-role-to-user admin developer -n ${project}"
 }
