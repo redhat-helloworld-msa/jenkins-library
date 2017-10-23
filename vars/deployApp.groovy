93565dcd2527828fe209831a406f1ca8eda756f9
Rafael Benevides <benevides@redhat.com>
2017-10-23 17:48:00 -0200

    Update deployApp.groovy

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -4,5 +4,4 @@ def call(project, app, tag){
     sh "oc new-app --name=${app} -n ${project} -l app=${app},hystrix.enabled=true --image-stream=${project}/${app}:${tag} || echo 'Aplication already Exists'"
     sh "oc expose service ${app} -n ${project} || echo 'Service already exposed'"
     sh "oc set probe dc/${app} -n ${project} --readiness --get-url=http://:8080/api/health"
-    sh "oc patch dc/${app} -n ${project} -p '{\"spec\":{\"template\":{\"spec\":{\"containers\":[{\"name\":\"${app}\",\"ports\":[{\"containerPort\": 8778,\"name\":\"jolokia\"}]}]}}}}'"
 }
