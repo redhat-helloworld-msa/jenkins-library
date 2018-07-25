9efecf1e4192d7c0786e2dc44236fcfe1ebaa8b4
Rafael Benevides <benevides@redhat.com>
2018-07-25 18:50:15 -0400

    Update canaryDeploy.groovy

--- vars/canaryDeploy.groovy
+++ vars/canaryDeploy.groovy
@@ -2,6 +2,6 @@
 
 def call(project, app, version){
     sh "oc new-app -n ${project} --name ${app}-${version} ${app}:${version} -l app=${app},deploymentconfig=${app},hystrix.enabled=true"
-    sh "oc set probe n ${project} dc/${app}-${version} --readiness --get-url=http://:8080/api/health"
+    sh "oc set probe -n ${project} dc/${app}-${version} --readiness --get-url=http://:8080/api/health"
     sh "oc patch svc/${app} -n ${project} -p '{\"spec\":{\"selector\":{\"app\": \"${app}\", \"deploymentconfig\": null}, \"sessionAffinity\":\"None\"}}' || echo 'Service ${app} already patched'"
 }
