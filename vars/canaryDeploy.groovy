cb40e7d961640cf34b1c9ac3a80100aaed460e15
Rafael Benevides <benevides@redhat.com>
2018-07-25 18:48:03 -0400

    Set Readiness Probe

--- vars/canaryDeploy.groovy
+++ vars/canaryDeploy.groovy
@@ -2,5 +2,6 @@
 
 def call(project, app, version){
     sh "oc new-app -n ${project} --name ${app}-${version} ${app}:${version} -l app=${app},deploymentconfig=${app},hystrix.enabled=true"
+    sh "oc set probe dc/${app} --readiness --get-url=http://:8080/api/health"
     sh "oc patch svc/${app} -n ${project} -p '{\"spec\":{\"selector\":{\"app\": \"${app}\", \"deploymentconfig\": null}, \"sessionAffinity\":\"None\"}}' || echo 'Service ${app} already patched'"
 }
